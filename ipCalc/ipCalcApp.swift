88e3efdb181bfd0574815eec2ee510218bd73ee6
Nathaniel Beach <nate.beach@gmail.com>
2023-04-07 11:44:31 -0700

    Initial Commit

---
+++ ipCalc/ipCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  ipCalcApp.swift
+//  ipCalc
+//
+//  Created by Nate Beach on 4/7/23.
+//
+
+import SwiftUI
+
+@main
+struct ipCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
